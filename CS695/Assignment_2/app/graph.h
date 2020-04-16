5e6e6915f31254e4ab04f2cd0a4d129b7df0d30c
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2020-04-16 22:07:06 +0530

    Add setVectorToDraw method

--- CS695/Assignment_2/app/graph.h
+++ CS695/Assignment_2/app/graph.h
@@ -5,8 +5,11 @@
 
 class Graph : public Gtk::DrawingArea {
    private:
-	std::string name;
-	enum { DRAW_HOLD, DRAW_START } state;
+	std::vector<int> mCurrState;
+	std::vector<int> mPrevState;
+	enum { DRAW_HOLD, DRAW_START } mState;
+
+	const int TIMEOUT_INTERVAL_IN_MILLIS = 1000;
 
    protected:
 	bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
@@ -15,12 +18,12 @@ class Graph : public Gtk::DrawingArea {
    public:
 	Graph();
 	~Graph();
-	void set_name(const std::string name) { this->name = std::move(name); }
+	void setVectorToDraw(const std::vector<int> vec);
 };
 
 Graph::Graph() {
 	Glib::signal_timeout().connect(sigc::mem_fun(*this, &Graph::on_timeout),
-								   1000);
+								   TIMEOUT_INTERVAL_IN_MILLIS);
 }
 
 Graph::~Graph() {}
@@ -36,34 +39,40 @@ bool Graph::on_timeout() {
 }
 
 bool Graph::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
-	// if (state == DRAW_HOLD) return true;
-
-	std::vector<int> a;
-	for (size_t i = 0; i < 40; i++) {
-		a.push_back(rand() % 101);
+	std::vector<int> vecToDraw;
+	if (mState == DRAW_HOLD) {
+		vecToDraw = mPrevState;
+	} else if (mState == DRAW_START) {
+		vecToDraw = mCurrState;
 	}
 
 	Gtk::Allocation allocation = get_allocation();
 	const int width = allocation.get_width();
 	const int height = allocation.get_height();
 
-	// coordinates for the center of the window
 	const int start_width = width / 101;
 	const int start_height = 5 * height / 110;
-	const int bin_width = width / a.size() + 1;
+	const int bin_width = width / vecToDraw.size() + 1;
 	const int bin_height = height / 110;
+
 	get_window()->freeze_updates();
 	cr->save();
 	cr->set_line_width(2.0);
 	cr->set_source_rgb(0.8, 0.0, 0.0);
 
-	for (size_t i = 0; i < a.size(); i++) {
+	for (size_t i = 0; i < vecToDraw.size(); i++) {
 		cr->line_to(start_width + bin_width * i,
-					height - (start_height + bin_height * a.at(i)));
+					height - (start_height + bin_height * vecToDraw.at(i)));
 	}
 
 	cr->stroke();
 	cr->restore();
 	get_window()->thaw_updates();
 	return true;
+}
+
+void Graph::setVectorToDraw(const std::vector<int> vec) {
+	mPrevState = std::move(mCurrState);
+	mCurrState = std::move(vec);
+	mState = DRAW_START;
 }
