d1dd501abd9e71264aa90bae9acdbdae6e3a0c6d
pranavchaudhary2310@gmail.com <pranavchaudhary2310@gmail.com>
2020-05-04 02:30:23 +0530

    Rearrange the folder structure

---
+++ CS695/Assignment_2/build-release/manager/CMakeFiles/Manager.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Manager.dir/Graph.cpp.o"
+  "CMakeFiles/Manager.dir/Manager.cpp.o"
+  "CMakeFiles/Manager.dir/VM.cpp.o"
+  "CMakeFiles/Manager.dir/VmManager.cpp.o"
+  "CMakeFiles/Manager.dir/main.cpp.o"
+  "Manager"
+  "Manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
